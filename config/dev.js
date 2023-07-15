340b7c1ec71ddc9a324bac1e0a840e08ee59223f
mr_baxa <bohodir29102001@gmail.com>
2023-07-15 12:46:23 +0500

    mongo url changes

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb_url:
-    "mongodb+srv://mr_baxa:bohodir29102001<>@everest-backend.4p1crg7.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://kh_zuck:67002001bx@cluster0.tqsixrv.mongodb.net/?retryWrites=true&w=majority",
   JWT_SECRET: "bohodir29102001<>",
   JWT_REFRESH_SECRET: "bohodir",
 };
